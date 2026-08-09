054f8288e4056c877ef1e0328ed165782016f895
polypixeldev <me@sfernandez.dev>
2026-08-08 23:20:32 -0400

    fixs

--- scripts/tobias.gd
+++ scripts/tobias.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOdwCITY = -1000.0
+const JUMP_VELOCITY = -1000.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
